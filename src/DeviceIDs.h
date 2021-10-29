82b23876a02748052c5d0dc1c244884c3c8b62ae
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2021-10-29 19:08:35 -0400

    fixed Bhagya's copy and paste errors

---
+++ src/DeviceIDs.h
@@ -0,0 +1,16 @@
+#ifndef DEVICEIDS_H
+#define DEVICEIDS_H
+
+namespace DeviceIDs
+{
+    static const int ExcvDrvLTal = 10; //****
+    static const int ExcvDrvRTal = 11; //****
+    static const int AugerTal = 12;
+    static const int canifier = 16;
+
+    static const int TPortDrvLTal = 21;
+
+
+};
+
+#endif
---
+++ src/MoveBase.cpp
@@ -0,0 +1,89 @@
+#include "ctre/Phoenix.h"
+#include "ros/ros.h"
+#include "std_msgs/Float32.h"
+#include "std_msgs/Int32.h"
+#include "geometry_msgs/Twist.h"
+#include "ctre/phoenix/platform/Platform.h"
+#include "ctre/phoenix/unmanaged/Unmanaged.h"
+#include "DeviceIDs.h"
+#include "ctre/phoenix/motorcontrol/SensorCollection.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace ctre::phoenix;
+using namespace ctre::phoenix::platform;
+using namespace ctre::phoenix::motorcontrol;
+using namespace ctre::phoenix::motorcontrol::can;
+
+//class to store variables and functions
+class Listener
+{
+    public:
+
+        Listener();
+        
+        //define functions to be used
+        void getTwistSpeed(const geometry_msgs::Twist twist);
+        void setMotorOutput();
+
+        //variables to store data used to drive left and right motorcontrollers
+        float leftPower = 0;
+        float rightPower = 0;
+
+        TalonSRX leftDrive = {DeviceIDs::ExcvDrvLTal};
+        TalonSRX rightDrive = {DeviceIDs::ExcvDrvRTal};
+};
+//function to make both wheels spin in the same direction
+Listener::Listener()
+{
+    rightDrive.SetInverted(true);
+}
+
+//function to use the data published by nav_stack and convert the data to motor input, then store the data
+void Listener::getTwistSpeed(const geometry_msgs::Twist twist)
+{
+    leftPower = 1 * twist.linear.x + 1 * twist.angular.z;
+    rightPower = 1 * twist.linear.x - 1 * twist.angular.z;
+}
+
+//function to send motor data to the motors
+void Listener::setMotorOutput()
+{
+    leftDrive.Set(ControlMode::PercentOutput, leftPower);
+    rightDrive.Set(ControlMode::PercentOutput, rightPower);
+
+    ctre::phoenix::unmanaged::FeedEnable(100);
+}
+
+//main function
+int main (int argc, char **argv)
+{
+    //startup ROS, required
+    ros::init(argc, argv, "MoveBase");
+    ros::NodeHandle n;
+    ros::Rate loop_rate(100);
+
+    Listener listener;
+
+    //connect cannable
+    phoenix::platform::can::SetCANInterface("can0");
+
+    //each time the nav_stack sends a message across this topic, call the function: getTwistSpeed().
+    //The message is a "twist" message; therefore, the parameter of the function is 
+    //getTwistSpeed(const geometry_msgs::Twist twist). This is defined above inside the listener class
+    ros::Subscriber twistMsg = n.subscribe("cmd_vel", 100, &Listener::getTwistSpeed, &listener);
+
+    //run this block of code as long as ROS is running
+    while (ros::ok())
+    {
+        //call the function to send power to the motor controllers
+        listener.setMotorOutput();
+
+        //needed for ROS to work properly
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
