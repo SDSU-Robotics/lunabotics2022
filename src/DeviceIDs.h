fb1115243835c4be67ba3636e1aadf566212f631
KaziKoIzak <izak.benitezlopez@jacks.sdstate.edu>
2022-01-15 16:02:00 -0600

    SavedLeifRefresh

---
+++ src/DeviceIDs.h
@@ -0,0 +1,10 @@
+#ifndef DEVICEIDS_H
+#define DEVICEIDS_H
+
+namespace DeviceIDs
+{
+    static const int motor = 1;
+
+};
+
+#endif
---
+++ src/JoyMap.h
@@ -0,0 +1,10 @@
+#ifndef JOYMAP_H
+#define JOYMAP_H
+
+namespace JoyMap
+{
+    static const int toggle = 0;
+
+};
+
+#endif
